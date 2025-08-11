6eaac05edd6a53afd642cd077f0f2af41c8fccab
Luiz Paulo Valença <luizpaulo165@gmail.com>
2025-08-11 20:22:58 -0300

    add partners

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -9,6 +9,51 @@ import {
 } from "../ui/dropdown-menu";
 import { Avatar, AvatarImage } from "../ui/avatar";
 
+const partners = [
+  {
+    name: "Playground",
+    image_dark: "/play-light.png",
+    image_light: "/play-dark.png",
+    storeLink: "https://www.playgroundgames.com.br/",
+  },
+  {
+    name: "Barão Geek House",
+    image_dark: "barao-light.png",
+    image_light: "barao-dark.png",
+    storeLink: "https://www.baraogeekhouse.com.br/",
+  },
+  {
+    name: "Dragon Skin",
+    image_dark: "dragon-light.png",
+    image_light: "dragon-dark.png",
+    storeLink: "https://dragonskin.com.br/",
+  },
+  {
+    name: "Pilha",
+    image_dark: "pilha-light.png",
+    image_light: "pilha-dark.png",
+    storeLink: "https://www.pilhacardgames.com/",
+  },
+  {
+    name: "TCG Box",
+    image_dark: "tcg-light.png",
+    image_light: "tcg-dark.png",
+    storeLink: "https://tcginbox.com.br/",
+  },
+  {
+    name: "Glórin",
+    image_dark: "glorin-light.png",
+    image_light: "grlorin-dark.png",
+    storeLink: "https://www.glorin.com.br/",
+  },
+  {
+    name: "LigaMagic",
+    image_dark: "liga-light.png",
+    image_light: "liga-dark.png",
+    storeLink: "https://www.ligamagic.com.br/?adLink=3ND",
+  },
+];
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { setTheme, theme } = useTheme();
 
@@ -48,22 +93,47 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
         </header>
       </div>
-      <main className="bg-background">{children}</main>
-      <div className="w-full p-4 text-center font-bold text-2xl">
-        <footer className="w-full p-4 text-center font-normal">
-          <span className="text-[14px] ">Ferramentra desenvolvida por</span>
-          <div className="*:data-[slot=avatar]:ring-background flex gap-2 flex items-center justify-center mt-2">
-            <div className="flex items-center flex-col ">
-              <Avatar>
-                <AvatarImage src="/jeff-avatar.jpg" />
-              </Avatar>
-              <span className="text-sm">Jeff3nd</span>
-            </div>
-            <div className="flex items-center justify-start flex-col ">
-              <Avatar>
-                <AvatarImage src="/papaulo-avatar.jpg" />
-              </Avatar>
-              <span className="text-sm">Papaulo</span>
+      <main className="bg-background min-h-5">{children}</main>
+      <div className="w-full  text-center font-bold text-2xl mt-20">
+        <div className="mb-20">
+          <span className="text-[14px] ">Nossos parceiros</span>
+          <div className="flex flex-wrap items-center justify-center gap-4">
+            {partners.map((partner) => (
+              <a
+                key={partner.name}
+                href={partner.storeLink}
+                target="_blank"
+                className="w-24 sm:w-auto"
+              >
+                <img
+                  src={`/partners/${
+                    theme === "dark" ? partner.image_light : partner.image_dark
+                  }`}
+                  alt={partner.name}
+                  className="inline-block w-32 object-contain"
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+        <footer className="w-full p-8 text-center font-normal bg-secondary">
+          <div className="w-full max-w-5xl mx-auto flex items-center justify-end">
+            <span className="text-[10px] ">Ferramentra desenvolvida por</span>
+            <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-2 pl-2">
+              <div className="flex items-center flex-col ">
+                <a href="https://www.instagram.com/jeff3nd/" target="_blank">
+                  <Avatar>
+                    <AvatarImage src="/jeff-avatar.jpg" />
+                  </Avatar>
+                </a>
+              </div>
+              <div className="flex items-center justify-start flex-col ">
+                <a href="https://bsky.app/profile/papaulo.bsky.social" target="_blank">
+                  <Avatar>
+                    <AvatarImage src="/papaulo-avatar.jpg" />
+                  </Avatar>
+                </a>
+              </div>
             </div>
           </div>
         </footer>
